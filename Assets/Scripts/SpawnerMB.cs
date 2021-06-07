1529b6087683fb9f02a62a28ddcd50e1d8d320e6
Lucas Sastre Camino G3 <gravixlsc@gmail.com>
2021-06-07 18:41:32 -0300

    Fixes

--- Assets/Scripts/SpawnerMB.cs
+++ Assets/Scripts/SpawnerMB.cs
@@ -6,7 +6,6 @@ public class SpawnerMB : MonoBehaviour
 {
     public GameObject prefab = null;
     public int maxSpawnCount;
-    public GameObject spawnPos;
     private List<GameObject> instances = new List<GameObject>();
     public int cantidadVivos;
 
@@ -24,7 +23,7 @@ public class SpawnerMB : MonoBehaviour
             spawnPos.z = transform.position.z;
 
             //////with random rotation
-            var newInstance = Instantiate(prefab, spawnPos, Quaternion.Euler(0, Random.value * 360, 0));
+            var newInstance = Instantiate(prefab, spawnPos, Quaternion.Euler(0, Random.value * 360, 0),transform);
             instances.Add(newInstance);
 
             // randomly delay before respawing
@@ -44,10 +43,21 @@ public class SpawnerMB : MonoBehaviour
 
     public void apagar(int cantidadVivos)
     {
+        float alpha = 1;
         while (instances.Count > cantidadVivos)
         {
-            Destroy(instances[0]);
+            //Destroy(instances[0]);
             instances.RemoveAt(0);
+            print(instances[0].GetComponentInChildren<MeshRenderer>().materials[0].name);
+            //instances[0].GetComponentInChildren<MeshRenderer>().materials[0].color = Color.green;
+
+            alpha = instances[0].GetComponentInChildren<MeshRenderer>().materials[0].color.a;
+
+            while (alpha > 0)
+            {
+                alpha -= 0.01f * Time.deltaTime;
+                instances[0].GetComponentInChildren<MeshRenderer>().materials[0].color = new Color(0, 0, 0, alpha);
+            }
         }
     }
 
